e1a83d18fd17c2b9f186fd035127af6d1612ba20
HYEONJEONG LEE <hlee220@myseneca.ca>
2025-03-20 17:55:25 +0900

    feat: react-query, store 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CounterStoreProvider } from "@/providers/counter-store-provider";
+import Providers from "@/providers/react-query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Providers>
         <CounterStoreProvider>
         {children}
         </CounterStoreProvider>
+      </Providers>
       </body>
     </html>
   );
