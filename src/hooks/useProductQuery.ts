f9cda5b96687958c35c75eee20cd29b187da6bee
HYEONJEONG LEE <hlee220@myseneca.ca>
2025-05-19 21:34:13 +0900

    fix: lint 버그 수정

--- src/hooks/useProductQuery.ts
+++ src/hooks/useProductQuery.ts
@@ -4,14 +4,14 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 export const useProductQuery = () => {
   return useInfiniteQuery({
     queryKey: ["products"],
-    queryFn: ({ pageParam = 0 }) => getProducts(pageParam),
+    queryFn: () => getProducts(),
     initialPageParam: 0,
     getNextPageParam: (lastPage) => lastPage.data.length > 0 ? lastPage.data[lastPage.data.length - 1].id : null,
   });
 }
 
 
-async function getProducts(pageParam: number) {
+async function getProducts() {
 	const res = await fetch(`http://localhost:4000/api/products`, {
 		cache: 'force-cache',
     credentials: 'include'
